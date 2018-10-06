1701c8a49d71b7721d4cc5d1236cb3d1e1057fc5
Wat01 <41803850+Wat01@users.noreply.github.com>
2018-10-07 01:38:06 +0200

    Update lifegame.cpp

--- lifegame.cpp
+++ lifegame.cpp
@@ -2,15 +2,16 @@
 #include <cstdlib>
 using namespace std;
 
-/**int counter(int plane[],int point[], int length)
-
-{return alives;}**/
+/**int counter(int plane[],int point[], int length[]){
+int alives = 0;
+return alives;}**/
 
 int main()
 {
 int filas, columnas;/// nvivas;
 cout<<"introduce el numero de filas: ";cin>>filas;
 columnas = filas;
+int length = [filas,columnas];
 int plane[filas][columnas];
 /**all points are randomly 0 or 1**/
 for(int i = 0; i<filas; i++){for(int k = 0; k<columnas; k++){plane[i][k] = rand() % 2;}}
@@ -20,7 +21,7 @@ cout<<endl;
 for(int i = 0; i<filas; i++){
 	for(int k = 0; k<filas; k++){
 		point[0] = i; point[1] = k;
-///counter(plane,point);
+counter(plane,point,length);
 cout<<"point value in plane: "<<plane[point[0]][point[1]]<<" point coordenates: ["<<point[0]<<","<<point[1]<<"]"<<endl;
 }}
 }
