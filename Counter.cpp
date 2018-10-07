7d9564e2fccbe1cc2e4984ade411a3fb1f6ac6f1
KpStuffOrigins <43894864+KpStuffOrigins@users.noreply.github.com>
2018-10-07 11:06:05 +0200

    Update Counter.cpp

--- Counter.cpp
+++ Counter.cpp
@@ -1,18 +1,13 @@
-int counter(int plane[][],int point[], int length[]){
+int counter(int plane[][], int length[]){
 	filas = length[0]; columnas = length[1];
 	int alive=0;
-		for(int j=0;j<columnas;j++){
-			for(int k=0;k<filas;k++){
-					if(plane[j][k]==1){
-							for(int i=0;i<2;i++){
-								if(plane[j+1][k+i]==1){alive++;}
-								if(plane[j-1][k-i]==1){alive++;}
-								if(plane[j+i][k+1]==1){alive++;}
-								if(plane[j-i][k-1]==1){alive++;}
-							}
-						}
-					}
-				}
-
+	if(plane[j][k]==1){
+		for(int i=0;i<2;i++){
+			if(plane[j+1][k+i]==1){alive++;}
+			if(plane[j-1][k-i]==1){alive++;}
+			if(plane[j+i][k+1]==1){alive++;}
+			if(plane[j-i][k-1]==1){alive++;}
+		}
+	}			
 	return alive;
 }
